5c713a41a358a30c656ddb169eaf1a736a34de58
Kermo Keerma <32697937+SyncMaster7@users.noreply.github.com>
2023-01-08 09:57:41 +0200

    Latest update

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int vowelCount = 0;
-        String[] vowelsList = {"a", "e", "i", "o", "u"};
-
-        System.out.println("Insert word or a sentence:");
-        String userWord = sc.nextLine();
-
-        // To simplify things, change userWord to loweCase.
-        String lowerCase = userWord.toLowerCase();
-        System.out.println(lowerCase);
-
-        for (int i = 0; i < userWord.length(); i++) {
-            if (lowerCase.contains(vowelsList[i])) {
-                vowelCount++;
-            }
-        }
-        System.out.println(vowelCount);
     }
 }
