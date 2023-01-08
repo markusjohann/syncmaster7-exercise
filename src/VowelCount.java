7f1065a9796126c20d0e7393a1289c03292b201a
Kermo Keerma <32697937+SyncMaster7@users.noreply.github.com>
2023-01-08 09:52:06 +0200

    Latest update

--- src/VowelCount.java
+++ src/VowelCount.java
@@ -6,13 +6,16 @@ public class VowelCount {
 
         Scanner sc = new Scanner(System.in);
         int vowelCount = 0;
-        String[] vowelsList = {"a", "e", "i", "o", "u"};
+//        String[] vowelsList = {"a", "e", "i", "o", "u"};
+//        char[] vowelsList = {'a', 'e', 'i', 'o', 'u'};
         String userSentence = sc.nextLine();
         String lowerCase = userSentence.toLowerCase();
 
-        for (int i = 0; i < userSentence.length(); i++) {
-            if (lowerCase.contains(vowelsList[i])) {
-                vowelCount++;
+        for (int i = 0; i < lowerCase.length(); i++) {
+
+          if(lowerCase.charAt(i) == 'a' || lowerCase.charAt(i) == 'e' || lowerCase.charAt(i) == 'i' ||
+             lowerCase.charAt(i) == 'o' || lowerCase.charAt(i) == 'u') {
+             vowelCount++;
             }
         }
         System.out.println(vowelCount);
